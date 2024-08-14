fbe18a587830fe02f06b00d762dec8f54824ee7e
Yriy Fedchenko <y.fedchenko@gmail.com>
2024-08-14 13:12:00 +0300

    add 404 page markup and styles for 375 px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import ProjectStatistics from "@/components/home/ProjectStatistics";
 import RoleSelectionLink from "@/components/home/RoleSelectionLink";
 import Testimonials from "@/components/home/Testimonials";
 import WhatWeOffer from "@/components/home/WhatWeOffer";
+import ErrorPage404 from "@/components/errorPage/errorPage404";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <Testimonials />
       <Faqs />
       <MyProfile />
+      <ErrorPage404 />
     </>
   );
 }
