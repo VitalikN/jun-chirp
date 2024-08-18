a1856b6786ae0ff8349a7d3c0140c2324cfe741c
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-08-18 10:20:37 +0300

    fix email

--- src/utils/schema/validationRegister.ts
+++ src/utils/schema/validationRegister.ts
@@ -13,9 +13,12 @@ export const validationSchemaRegister = Yup.object().shape({
   email: Yup.string()
     .email("Некоректний формат електронної пошти")
     .required("Поле електронної пошти не може бути порожнім")
-
     .matches(
       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Електронна адреса повинна містити тільки латинські літери"
+    )
+    .matches(
+      /^[^\s@]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
       "Введіть дійсну електронну адресу у форматі username@example.com"
     ),
 
--- src/utils/schema/validationSchemaSignIn.ts
+++ src/utils/schema/validationSchemaSignIn.ts
@@ -4,9 +4,13 @@ export const validationSchemaSignIn = Yup.object().shape({
   email: Yup.string()
     .email("Некоректний формат електронної пошти")
     .required("Поле електронної пошти не може бути порожнім")
-
     .matches(
       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Електронна адреса повинна містити тільки латинські літери"
+    )
+    .matches(
+      /^[^\s@]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+
       "Введіть дійсну електронну адресу у форматі username@example.com"
     ),
   password: Yup.string()
