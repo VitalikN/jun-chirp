e09570489ea2f0544a2ba1ba0b42241d1dcfa963
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-08-22 09:58:17 +0300

    + added faq/support components and styles

---
+++ src/components/home/Faqs/faqsText.ts
@@ -0,0 +1,17 @@
+export const faqs = [
+  {
+    question: "What’s the most frequently asked question?",
+    answer:
+      "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.",
+  },
+  {
+    question: "How about a second one?",
+    answer:
+      "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.",
+  },
+  {
+    question: "And a third?",
+    answer:
+      "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.",
+  },
+];
