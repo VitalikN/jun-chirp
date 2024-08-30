b8be7ba1670ffc5bae380ff6a146ddec3d058e4b
Eternal-Love-and-Fire <yevgenkotyk@gmail.com>
2024-08-30 17:33:46 +0300

    add; password indicator bar

--- src/components/auth/PasswordStrengthIndicator/PasswordStrengthIndicator.tsx
+++ src/components/auth/PasswordStrengthIndicator/PasswordStrengthIndicator.tsx
@@ -3,24 +3,22 @@ import s from "../Register/register.module.scss";
 
 interface Indicator {
   password: string;
-  userName: string;
 }
 
-const PasswordStrengthIndicator = ({ password, userName }: Indicator) => {
-  const { strength, getBarStyle, getTextColor } = usePasswordStrength({
+const PasswordStrengthIndicator = ({ password }: Indicator) => {
+  const { strength, barStyle, color } = usePasswordStrength({
     password,
-    userName,
   });
-  console.log(strength)
+  console.log(strength);
 
   return (
     <div className={s.passwordStrength}>
-      <div className={s.strengthBar} style={getBarStyle(strength)}></div>
+      <div className={s.strengthBar} style={barStyle}></div>
       <div className={s.strengthLevels}>
         <span
           className={strength === "weak" ? s.active : ""}
           style={{
-            color: strength === "weak" ? getTextColor("weak") : "#616161",
+            color: strength === "weak" ? color : "#616161",
           }}
         >
           Легкий
@@ -28,7 +26,7 @@ const PasswordStrengthIndicator = ({ password, userName }: Indicator) => {
         <span
           className={strength === "medium" ? s.active : ""}
           style={{
-            color: strength === "medium" ? getTextColor("medium") : "#616161",
+            color: strength === "medium" ? color : "#616161",
           }}
         >
           Середній
@@ -36,7 +34,7 @@ const PasswordStrengthIndicator = ({ password, userName }: Indicator) => {
         <span
           className={strength === "strong" ? s.active : ""}
           style={{
-            color: strength === "strong" ? getTextColor("strong") : "#616161",
+            color: strength === "strong" ? color : "#616161",
           }}
         >
           Складний
