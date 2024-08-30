f2d0f37fadad5bf3c5dff638fe1c9ad08d716a1c
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-08-30 16:57:26 +0300

    fix email

--- src/components/auth/ResetPassword/validationSchemaResetPassword.ts
+++ src/components/auth/ResetPassword/validationSchemaResetPassword.ts
@@ -9,15 +9,9 @@ export const validationSchemaResetPassword = Yup.object().shape({
     .email("Некоректний формат електронної пошти")
     .required("Поле електронної пошти не може бути порожнім")
     .matches(
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Введіть дійсну електронну адресу у форматі username@example.com"
-    )
-    .matches(
-      /^[^\s@]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*(\.[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*)*\.[a-zA-Z]{2,}$/,
       "Введіть дійсну електронну адресу у форматі username@example.com"
     ),
-
   newPassword: Yup.string()
     .min(8, "Пароль повинен містити щонайменше 8 символів")
     .max(20, "Пароль може містити не більше 20 символів")
